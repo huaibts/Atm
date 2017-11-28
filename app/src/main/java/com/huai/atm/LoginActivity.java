c80257c37ebe3d2f5d9c917a588413599e2cb396
huaijung Huang <je3rjoy@gmail.com>
2017-11-28 11:05:22 +0800

    login layout

--- app/src/main/java/com/huai/atm/LoginActivity.java
+++ app/src/main/java/com/huai/atm/LoginActivity.java
@@ -2,12 +2,22 @@ package com.huai.atm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public void login (View view){
+
+    }
+    public void cancel (View view){
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
