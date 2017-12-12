19693166f98071e0bf629ce08036baddaa9e76dc
huaijung Huang <je3rjoy@gmail.com>
2017-12-12 12:06:58 +0800

    add

--- app/src/main/java/com/huai/atm/UserInfoActivity.java
+++ app/src/main/java/com/huai/atm/UserInfoActivity.java
@@ -2,6 +2,7 @@ package com.huai.atm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class UserInfoActivity extends AppCompatActivity {
@@ -11,7 +12,7 @@ public class UserInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
     }
-    public void ok(){
+    public void ok(View view){
         EditText edNickname = (EditText) findViewById(R.id.ed_nickname);
         EditText edPhone = (EditText) findViewById(R.id.ed_phone);
         String nickname = edNickname.getText().toString();
