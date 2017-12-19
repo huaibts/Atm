a5c82990e34b7de5f8be894bfe7b5623b6289f7c
huaijung Huang <je3rjoy@gmail.com>
2017-12-19 11:47:16 +0800

    Ch8

--- app/src/main/java/com/huai/atm/UserInfoActivity.java
+++ app/src/main/java/com/huai/atm/UserInfoActivity.java
@@ -2,12 +2,21 @@ package com.huai.atm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class UserInfoActivity extends AppCompatActivity {
+    private static final String TAG = UserInfoActivity.class.getSimpleName();
     private EditText edNickname;
     private EditText edPhone;
+    private Spinner ages;
+    private ArrayAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,15 +29,26 @@ public class UserInfoActivity extends AppCompatActivity {
                 .getString("PHONE", "");
         edNickname.setText(nickname);
         edPhone.setText(phone);
+        ages = (Spinner) findViewById(R.id.ages);
+       // ArrayList<String> data = new ArrayList<>();
+       // for (int i = 15; i<=40; i++){
+       //     data.add(i+"");
+       // }
+       /* ArrayAdapter adapter = new ArrayAdapter(
+                this, android.R.layout.simple_list_item_1,data);*/
+       ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+               R.array.ages, android.R.layout.simple_list_item_1);
+        ages.setAdapter(adapter);
     }
     public void ok(View view){
+        Log.d(TAG, "ok: "+ages.getSelectedItem().toString());
+        int age = Integer.parseInt(ages.getSelectedItem().toString());
         String nickname = edNickname.getText().toString();
         String phone = edPhone.getText().toString();
-        if ("jack".equals(nickname) && "0912345678".equals(phone)) {
-            getIntent().putExtra("EXTRA_NICKNAME", nickname);
-            getIntent().putExtra("EXTRA_PHONE", phone);
-            setResult(RESULT_OK, getIntent());
-            finish();
-        }
+        getIntent().putExtra("EXTRA_NICKNAME", nickname);
+        getIntent().putExtra("EXTRA_PHONE", phone);
+        setResult(RESULT_OK, getIntent());
+        finish();
+
     }
 }
